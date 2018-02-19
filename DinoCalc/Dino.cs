1b0b24cc40070d34f9b50e860cdab0e805dc8050
rudek1212 <Rudnickibartosz95@gmail.com>
2018-02-19 22:11:54 +0100

    Prototype

---
+++ DinoCalc/Dino.cs
@@ -0,0 +1,17 @@
+﻿namespace DinoCalc
+{
+    internal class Dino
+    {
+        public int hp;
+
+        public Dino()
+        {
+            hp = 5;
+        }
+
+        public void Reduce()
+        {
+            hp--;
+        }
+    }
+}
--- DinoCalc/Program.cs
+++ DinoCalc/Program.cs
@@ -10,6 +10,66 @@ namespace DinoCalc
     {
         static void Main(string[] args)
         {
+            Calc2();
+
+        }
+
+        public static void Calc()
+        {
+            Console.WriteLine("How many iterations You want to calculate?:");
+            int iterations = Convert.ToInt32(Console.ReadLine());
+
+            List<Dino> list = new List<Dino>();
+            list.Add(new Dino());
+            var dinoDeath = 0;
+            for (int i = 0; i < iterations; i++)
+            {
+                var tmp = list.Count;
+                foreach (var dino in list)
+                {
+                    dino.hp--;
+                }
+                for (int j = tmp - 1; j >= 0; j--)
+                {
+                    if (list[j].hp == 0)
+                    {
+                        list.RemoveAt(j);
+                        dinoDeath++;
+                    }
+                }
+                for (int j = 0; j < tmp; j++)
+                {
+                    list.Add(new Dino());
+                }
+                Console.WriteLine("Iteration:{0}, number of raptors:{1}, number of dead raptors:{2}", i + 1, list.Count,
+                    dinoDeath);
+            }
+            Console.ReadKey();
+        }
+
+        public static void Calc2()
+        {
+            Console.WriteLine("How many iterations You want to calculate?:");
+            int iterations = Convert.ToInt32(Console.ReadLine());
+            ulong dino5 = 1;
+            ulong dino4 = 0;
+            ulong dino3 = 0;
+            ulong dino2 = 0;
+            ulong dino1 = 0;
+            ulong dinoDead = 0;
+            for (int i = 0; i < iterations; i++)
+            {
+                ulong tmpAlive = dino5 + dino4 + dino3 + dino2 + dino1;
+                dinoDead = dino1;
+                dino1 = dino2;
+                dino2 = dino3;
+                dino3 = dino4;
+                dino4 = dino5;
+                dino5 = tmpAlive;
+                Console.WriteLine("Iteration:{0}, number of raptors:{1}, number of dead raptors:{2}", i + 1, dino1 + dino2 + dino3 + dino4 + dino5, dinoDead);
+                Console.WriteLine(dino1+dino2+dino3+dino4+dino5);
+            }
+            Console.ReadLine();
         }
     }
 }
